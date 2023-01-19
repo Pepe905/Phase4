fe7a3abe84bb7e00a5c34be6d198f0e9a1a35d5f
Pepe905 <marcelpospiech@googlemail.com>
2023-01-19 18:12:44 +0100

    ++ Uprade 5.1

--- Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSessions/Definitions.AdvancedSessions.h
+++ Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSessions/Definitions.AdvancedSessions.h
@@ -34,14 +34,14 @@
 #define WITH_PUSH_MODEL 0
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
-#define UE_LIVE_CODING_ENGINE_DIR "C:\\Program Files\\Epic Games\\UE_4.27\\Engine"
-#define UE_LIVE_CODING_PROJECT "C:\\Users\\morde\\Documents\\Unreal Projects\\PluginExpProject\\VRExpPluginExample.uproject"
+#define UE_LIVE_CODING_ENGINE_DIR "C:\\UE4\\UE_4.27\\Engine"
+#define UE_LIVE_CODING_PROJECT "D:\\Documents\\Unreal Projects\\VRExpPluginExp\\VRExpPluginExample.uproject"
 #define UBT_MODULE_MANIFEST "UE4.modules"
 #define UBT_MODULE_MANIFEST_DEBUGGAME "UE4-Win64-DebugGame.modules"
 #define UBT_COMPILED_PLATFORM Win64
 #define UBT_COMPILED_TARGET Game
 #define UE_APP_NAME "UE4"
-#define UE_ENGINE_DIRECTORY "../../../../../../../Program Files/Epic Games/UE_4.27/Engine/"
+#define UE_ENGINE_DIRECTORY "C:/UE4/UE_4.27/Engine/"
 #define NDIS_MINIPORT_MAJOR_VERSION 0
 #define WIN32 1
 #define _WIN32_WINNT 0x0601
